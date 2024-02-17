ae26a36f5e142e5250f84197d998ce1584d77375
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-02-17 05:57:15 +0000

    Operations discussed

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "true"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "sahnawas"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(BooleanisLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
