262464dd0ba51a9e75148554eba97fc8d099744e
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-02-17 04:04:48 +0000

    Data types declared

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 155355
+let accountEmail = "harsh@gmail.com"
+var accountPassword = "768986"
+accountCity = "jaipur"
+
+accountEmail = "beniwal@gmail.com"
+accountPassword = "545654"
+accountCity = "delhi"
+
+
+// prefer not to use var
+
+
+console.table([accountEmail, accountPassword, accountCity])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; //treats all the js code as newer version
+
+alert("hello") //this will not work because we are using node js not browser
