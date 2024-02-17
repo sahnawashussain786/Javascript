310274d74d2e2b3934edfd61ccd95b0b55b4fbcf
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-02-17 04:31:03 +0000

    Data types learned

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -1,3 +1,5 @@
 "use strict"; //treats all the js code as newer version
 
-alert("hello") //this will not work because we are using node js not browser
+const email = 33
+
+console.log(typeof null);
